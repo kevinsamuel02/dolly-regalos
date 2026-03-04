b5bc361ba335406d8a823a3ce969facd490c2c3b
Kevin Samuel <ksamuel@frba.utn.edu.ar>
2026-03-04 13:31:25 -0300

    Correccion de error en las fotos

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import './App.css';
 
 function App() {
   const productos = [
-    { id: 'T01', nombre: 'Toallón', colores: 'Negro, Beige, Crudo, Blanco', imagen: '/img/toallon.jpeg' },
-    { id: 'P01', nombre: 'Pareo', colores: 'Marrón, Negro, Blanco', imagen: '/img/pareo.jpeg' },
-    { id: 'V01', nombre: 'Vincha', colores: 'Blanco, Crudo, Lila, Marrón, Rosa', imagen: '/img/vincha.jpeg' },
-    { id: 'B01', nombre: 'Babero', colores: 'Negro, Blanco', imagen: '/img/babero.jpeg' },
-    { id: 'T05', nombre: 'Toalla de mano', colores: 'Blanco, Gris, Rosa, Beige', imagen: '/img/toallita.jpg' }
+    { id: 'T01', nombre: 'Toallón', colores: 'Negro, Beige, Crudo, Blanco', imagen: 'img/toallon.jpeg' },
+    { id: 'P01', nombre: 'Pareo', colores: 'Marrón, Negro, Blanco', imagen: 'img/pareo.jpeg' },
+    { id: 'V01', nombre: 'Vincha', colores: 'Blanco, Crudo, Lila, Marrón, Rosa', imagen: 'img/vincha.jpeg' },
+    { id: 'B01', nombre: 'Babero', colores: 'Negro, Blanco', imagen: 'img/babero.jpeg' },
+    { id: 'T05', nombre: 'Toalla de mano', colores: 'Blanco, Gris, Rosa, Beige', imagen: 'img/toallita.jpg' }
   ];
 
   const abrirWhatsApp = (nombreProducto) => {
@@ -19,7 +19,7 @@ function App() {
   return (
     <div>
       <header className="header">
-        <img src="/img/logo.png" alt="Dolly Regalos Logo" className="logo-img" />
+        <img src="img/logo.png" alt="Dolly Regalos Logo" className="logo-img" />
         <h2>Bordados Personalizados</h2>
         <p>Toallas, toallones, delantales y más, con el diseño que vos elijas.</p>
       </header>
